6781d2238c665cc2429e87d3b3d4fd5f8789e635
makaspacex <makaspacex@outlook.com>
2024-05-12 19:35:25 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import type { Theme } from 'vitepress'
 
 import elementplus from "element-plus"
 import "element-plus/dist/index.css"
-import * as ElementPlusIconsVue from "@element-plus/icons-vue"
+// import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 
 // 自定义样式重载
 import "./style.scss"
@@ -17,8 +17,8 @@ export default {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
     app.use(elementplus)
-    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-      app.component(key, component)
-    }
+    // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    //   app.component(key, component)
+    // }
   },
 } satisfies Theme
