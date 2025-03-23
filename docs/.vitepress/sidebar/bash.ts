e731e14097b389f74af3bef8dff26e35f10f9f6b
makaspacex <makaspacex@outlook.com>
2025-03-23 22:32:34 +0800

    修复导航链接

--- docs/.vitepress/sidebar/bash.ts
+++ docs/.vitepress/sidebar/bash.ts
@@ -1,31 +1,32 @@
 
 export default function sidebarBash() {
-  return [
+   return [
       {
-      items: [
-         { text:'1.简介', link: 'programer/bash/01-intro' },
-        { text:'2.基本语法', link: 'programer/bash/02-grammar' },
-        { text:'3.模式扩展', link: 'programer/bash/03-expansion' },
-        { text:'4.引号和转义', link: 'programer/bash/04-quotation' },
-        { text:'5.变量', link: 'programer/bash/05-variable' },
-        { text:'6.字符串操作', link: 'programer/bash/06-string' },
-        { text:'7.算术运算', link: 'programer/bash/07-arithmetic' },
-        { text:'8.操作历史', link: 'programer/bash/08-history' },
-        { text:'9.行操作', link: 'programer/bash/09-readline' },
-        { text:'10.目录堆栈', link: 'programer/bash/10-stack' },
-        { text:'11.脚本入门', link: 'programer/bash/11-script' },
-        { text:'12.read 命令', link: 'programer/bash/12-read' },
-        { text:'13.条件判断', link: 'programer/bash/13-condition' },
-        { text:'14.循环', link: 'programer/bash/14-loop' },
-        { text:'15.函数', link: 'programer/bash/15-function' },
-        { text:'16.数组', link: 'programer/bash/16-array' },
-        { text:'17.set 命令，shopt 命令', link: 'programer/bash/17-set' },
-        { text:'18.脚本除错', link: 'programer/bash/18-debug' },
-        { text:'19.mktemp 命令，trap 命令', link: 'programer/bash/19-mktemp' },
-        { text:'20.启动环境', link: 'programer/bash/20-startup' },
-        { text:'21.命令提示符', link: 'programer/bash/21-prompt' },
- 
-]
+         text: 'Bash教程',
+         items: [
+            { text: '1.简介', link: '/programer/bash/01-intro' },
+            { text: '2.基本语法', link: '/programer/bash/02-grammar' },
+            { text: '3.模式扩展', link: '/programer/bash/03-expansion' },
+            { text: '4.引号和转义', link: '/programer/bash/04-quotation' },
+            { text: '5.变量', link: '/programer/bash/05-variable' },
+            { text: '6.字符串操作', link: '/programer/bash/06-string' },
+            { text: '7.算术运算', link: '/programer/bash/07-arithmetic' },
+            { text: '8.操作历史', link: '/programer/bash/08-history' },
+            { text: '9.行操作', link: '/programer/bash/09-readline' },
+            { text: '10.目录堆栈', link: '/programer/bash/10-stack' },
+            { text: '11.脚本入门', link: '/programer/bash/11-script' },
+            { text: '12.read 命令', link: '/programer/bash/12-read' },
+            { text: '13.条件判断', link: '/programer/bash/13-condition' },
+            { text: '14.循环', link: '/programer/bash/14-loop' },
+            { text: '15.函数', link: '/programer/bash/15-function' },
+            { text: '16.数组', link: '/programer/bash/16-array' },
+            { text: '17.set 命令，shopt 命令', link: '/programer/bash/17-set' },
+            { text: '18.脚本除错', link: '/programer/bash/18-debug' },
+            { text: '19.mktemp与trap', link: '/programer/bash/19-mktemp' },
+            { text: '20.启动环境', link: '/programer/bash/20-startup' },
+            { text: '21.命令提示符', link: '/programer/bash/21-prompt' },
+
+         ]
       }
    ]
 }
--- docs/.vitepress/sidebar/typescript.ts
+++ docs/.vitepress/sidebar/typescript.ts
@@ -1,36 +1,37 @@
 
 export default function sidebarTypescript() {
-  return [
+   return [
       {
-      items: [
-         { text:'1.简介', link: 'programer/typescript/01-intro' },
-        { text:'2.基本用法', link: 'programer/typescript/02-basic' },
-        { text:'3.any 类型', link: 'programer/typescript/03-any' },
-        { text:'4.类型系统', link: 'programer/typescript/04-types' },
-        { text:'5.数组', link: 'programer/typescript/05-array' },
-        { text:'6.元组', link: 'programer/typescript/06-tuple' },
-        { text:'7.symbol 类型', link: 'programer/typescript/07-symbol' },
-        { text:'8.函数', link: 'programer/typescript/08-function' },
-        { text:'9.对象', link: 'programer/typescript/09-object' },
-        { text:'10.interface', link: 'programer/typescript/10-interface' },
-        { text:'11.类', link: 'programer/typescript/11-class' },
-        { text:'12.泛型', link: 'programer/typescript/12-generics' },
-        { text:'13.Enum 类型', link: 'programer/typescript/13-enum' },
-        { text:'14.类型断言', link: 'programer/typescript/14-assert' },
-        { text:'15.模块', link: 'programer/typescript/15-module' },
-        { text:'16.namespace', link: 'programer/typescript/16-namespace' },
-        { text:'17.装饰器', link: 'programer/typescript/17-decorator' },
-        { text:'18.装饰器（旧语法）', link: 'programer/typescript/18-decorator-legacy' },
-        { text:'19.declare 关键字', link: 'programer/typescript/19-declare' },
-        { text:'20.d.ts 类型声明文件', link: 'programer/typescript/20-d.ts' },
-        { text:'21.类型运算符', link: 'programer/typescript/21-operator' },
-        { text:'22.类型映射', link: 'programer/typescript/22-mapping' },
-        { text:'23.类型工具', link: 'programer/typescript/23-utility' },
-        { text:'24.注释指令', link: 'programer/typescript/24-comment' },
-        { text:'25.tsconfig.json 文件', link: 'programer/typescript/25-tsconfig.json' },
-        { text:'26.tsc 命令', link: 'programer/typescript/26-tsc' },
- 
-]
+         text: 'TS教程',
+         items: [
+            { text: '1.简介', link: '/programer/typescript/01-intro' },
+            { text: '2.基本用法', link: '/programer/typescript/02-basic' },
+            { text: '3.any 类型', link: '/programer/typescript/03-any' },
+            { text: '4.类型系统', link: '/programer/typescript/04-types' },
+            { text: '5.数组', link: '/programer/typescript/05-array' },
+            { text: '6.元组', link: '/programer/typescript/06-tuple' },
+            { text: '7.symbol 类型', link: '/programer/typescript/07-symbol' },
+            { text: '8.函数', link: '/programer/typescript/08-function' },
+            { text: '9.对象', link: '/programer/typescript/09-object' },
+            { text: '10.interface', link: '/programer/typescript/10-interface' },
+            { text: '11.类', link: '/programer/typescript/11-class' },
+            { text: '12.泛型', link: '/programer/typescript/12-generics' },
+            { text: '13.Enum 类型', link: '/programer/typescript/13-enum' },
+            { text: '14.类型断言', link: '/programer/typescript/14-assert' },
+            { text: '15.模块', link: '/programer/typescript/15-module' },
+            { text: '16.namespace', link: '/programer/typescript/16-namespace' },
+            { text: '17.装饰器', link: '/programer/typescript/17-decorator' },
+            { text: '18.装饰器（旧语法）', link: '/programer/typescript/18-decorator-legacy' },
+            { text: '19.declare 关键字', link: '/programer/typescript/19-declare' },
+            { text: '20.d.ts 类型声明文件', link: '/programer/typescript/20-d.ts' },
+            { text: '21.类型运算符', link: '/programer/typescript/21-operator' },
+            { text: '22.类型映射', link: '/programer/typescript/22-mapping' },
+            { text: '23.类型工具', link: '/programer/typescript/23-utility' },
+            { text: '24.注释指令', link: '/programer/typescript/24-comment' },
+            { text: '25.tsconfig.json 文件', link: '/programer/typescript/25-tsconfig.json' },
+            { text: '26.tsc 命令', link: '/programer/typescript/26-tsc' },
+
+         ]
       }
    ]
 }
